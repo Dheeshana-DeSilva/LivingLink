bc8e0a5e7afde862cb9096eb3ba780b3e25e030c
Dheeshana De Silva <dheeshanadesilva2002@gmail.com>
2026-05-23 03:31:18 +0530

    feat: add api gateway

---
+++ backend/api-gateway/src/test/java/com/livinglink/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.livinglink.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
